595448b968b37773b7a1236cbdf98b5665047c07
zhipengwu90 <zhipeng.wu@itas.ca>
2023-01-20 13:38:29 -0800

    add tech-stack/context diagram

--- App.test.js
+++ App.test.js
@@ -9,3 +9,11 @@ describe('<App />', () => {
     expect(tree.children.length).toBe(1);
   });
 });
+
+// test('renders correctly', () => {
+//     const tree = renderer.create(<App />).toJSON();
+//     expect(tree).toMatchSnapshot();
+//   });
+test('two plus two is four', () => {
+    expect(2 + 2).toBe(4);
+  });
